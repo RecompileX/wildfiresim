d626c8a405aa972ef8db4b1927d92216695c467e
Hamza Kamal <hamzakamal643@gmail.com>
2026-08-31 22:19:46 -0500

    minor bug fixes

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ int main(){
         if(inSettings == false){
             ClearBackground(RED);
         
-            DrawText("Wildfire Sim v0.3", sWidth/2 - .5 * (MeasureText("Wildfire sim  v0.1", 60)), .25 * sHeight, 60, WHITE);
+            DrawText("Wildfire Sim v0.3.1", sWidth/2 - .5 * (MeasureText("Wildfire sim  v0.3.1", 60)), .25 * sHeight, 60, WHITE);
             DrawText("Select map", sWidth/2 - .5 * (MeasureText("Select map", 40)), .4 * sHeight, 40, WHITE);
             for(int x = 0; x < mapList.size(); x++){
                 if(GuiButton((Rectangle){25 + ((sWidth - 50) / mapList.size()) * x, sHeight / 2, (sWidth - 50) / mapList.size(), .1 * sHeight}, mapList[x].c_str())){
